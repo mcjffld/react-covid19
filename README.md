7e83a9f965f588bdfc50d9941ae4a9c687ad6fce
Michael James <michael.james@priceline.com>
2020-06-29 01:04:22 -0400

    v0

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Victory Tutorial
-A tutorial for Victory used with the Getting Started guide in Victory Docs. Build a stacked bar chart with Victory components.
-
-## Getting Started
-Clone down the repo, run an `npm install` to install dependencies, and then use `npm start` to run the webpack server. The server will run at `localhost:8080`.
+React and COVID19
--- src/js/client.js
+++ src/js/client.js
@@ -1,80 +1,120 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+import { VictoryChart, VictoryLine, VictoryAxis, VictoryTheme } from 'victory';
+import _ from 'lodash'
+import currentWeekNumber from 'current-week-number'
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    };
+  }
 
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
-];
+  tick() {
+    const stateKey = _.sample(this.keys)
+    console.log(stateKey)
+    this.setState({key: stateKey})
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  
+  setKeys( keys ) {
+    this.keys = keys
+  }
 
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
+  componentDidMount() {
+    fetch("https://data.cdc.gov/resource/r8kw-7aab.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          const data = {}
+          
+          _.forEach(result, function(item) {
+            if (!data[item["state"]]) {
+              data[item["state"]] = [ item ]
+              console.log(item["state"])
+            } else {
+              data[item["state"]].push(item)
+            }
+            item.x = Number(currentWeekNumber(item["start_week"]))
+            item.y = Number(item["covid_deaths"]) || 0
+          });
+          this.setKeys(Object.keys(data))
 
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
-];
+          this.setState({
+            isLoaded: true,
+            data: data,
+            key: "United States"
+          });
+          this.timerID = setInterval(
+            () => this.tick(),
+            5000
+          );
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, data, key } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <VictoryChart 
+          theme={VictoryTheme.material}
+          style={{
+            parent: {
+              border: "1px solid #ccc"
+            }
+           }}
+           responsive={false}
+           >
+        <VictoryLine data={data[key]}/>  
+        <VictoryAxis
+          label="Week Number"
+          style={{
+            axisLabel: { padding: 30 }
+          }}
+        />
+        <VictoryAxis dependentAxis
+          label={ `COVID 19 Deaths in ${key}` }
+          style={{
+            axisLabel: { padding: 35}
+          }}
+        />
+        </VictoryChart>
+      );
+    }
+  }
+}
 
 class Main extends React.Component {
+
   render() {
     return (
-      <div>
-        <h1>Victory Tutorial</h1>
-        <VictoryChart
-          domainPadding={10}
-          theme={VictoryTheme.material}
-        >
-          <VictoryAxis
-            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-          />
-          <VictoryAxis
-            dependentAxis
-            tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-          <VictoryStack
-            colorScale={"warm"}
-          >
-            <VictoryBar
-              data={data2012}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2013}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2014}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2015}
-              x={"quarter"}
-              y={"earnings"}
-            />
-          </VictoryStack>
-        </VictoryChart>
+      <div className="data" style={{width: 650 + 'px'}} >
+        <Chart/>
       </div>
     );
   }
 }
 
 const app = document.getElementById('app');
+
 ReactDOM.render(<Main />, app);
